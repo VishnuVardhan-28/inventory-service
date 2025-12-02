39901847b74dd80502260e7a6974e6fabf433825
VishnuVardhan-28 <dhanduvishnu@gmail.com>
2025-12-02 23:04:41 +0530

    implement WebClient Communication with order-service

--- src/main/java/com/microservices/app/inventory_service/controller/InventoryController.java
+++ src/main/java/com/microservices/app/inventory_service/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.microservices.app.inventory_service.controller;
 
+import com.microservices.app.inventory_service.dto.InventoryResponse;
 import com.microservices.app.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,10 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    //  http://localhost:8082/api/inventory?skuCode=mutton&skuCode=chicken
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 
